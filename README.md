669dbeadc160241de616f637776c8ff9342789dc
malvin_jay <george@plendify.com>
2023-04-22 19:29:32 +0000

    changed app name from untitled to meeting-app

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 ## Development server
 
-Run `nx serve untitled` for a dev server. Navigate to http://localhost:10001/. The app will automatically reload if you change any of the source files.
+Run `nx serve meeting-app` for a dev server. Navigate to http://localhost:10001/. The app will automatically reload if you change any of the source files.
 
 ## Understand this workspace
 
--- apps/meeting-app-e2e/src/e2e/app.cy.ts
+++ apps/meeting-app-e2e/src/e2e/app.cy.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('untitled', () => {
+describe('meeting-app', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('untitled', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome untitled');
+    getGreeting().contains('Welcome meeting-app');
   });
 });
