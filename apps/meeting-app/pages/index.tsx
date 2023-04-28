9091c50b690c03b97af14bbb0011ccb9bd329bf6
malvin_jay <george@plendify.com>
2023-04-28 11:22:13 +0000

    Updated env files

--- apps/meeting-app/pages/index.tsx
+++ apps/meeting-app/pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
-import DyteClient from '@dytesdk/web-core';
 import Link from 'next/link';
+
+import DyteClient from '@dytesdk/web-core';
 import { DyteProvider, useDyteClient } from '@dytesdk/react-web-core';
 import { DyteMeeting } from '@dytesdk/react-ui-kit';
 
@@ -10,7 +11,7 @@ import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { Button, Loader } from '@alpha/ui';
 import { LogOut } from 'react-feather';
 
-const token = process.env.NEXT_PUBLIC_AUTH_TOKEN || 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdJZCI6ImExYjAxZGVkLTk2Y2EtNDJjOC05ZTkwLTRiMjY4MzY2NmQ5YiIsIm1lZXRpbmdJZCI6ImJiYjJkMmVlLWQ4OTctNDE3My05ZWNiLTI0MzQ1OTA5MmQ3YSIsInBhcnRpY2lwYW50SWQiOiJhYWEyNjA2YS1lODdkLTQxODQtOGIzMi01NGQxMzBjNmYyZDEiLCJwcmVzZXRJZCI6Ijg4MmZhMTgyLWZiNWEtNDQwYy05ZmQwLWEzZWExODA2NzZlNSIsImlhdCI6MTY4MjExNjU1MSwiZXhwIjoxNjkwNzU2NTUxfQ.dvIB19jnaSEBrAeCGwJ2EhPbzcNrRfnngOhOhhnVOi5XNCuAetwpdbJPq9cIeHzbGXmaECfw_dS5pifgqIo3IU4G32hCDhu-XkDsp4jOFx97fNnWcDu7mN-WYhn0WSvGrTRcl1BIo33oCPidkt_ZaLvUIKs5WcyoHCymRKg1qpyebFwce0fOWHuyWkGARn6uURqF15DncQ6UofgFIU07s8ZyE87porM-dVOrIosJoWFJ4xe9SlNgbFaTlq7AgT9_6KtpyKU5-ddhcAZ1NgAZJJY-uuOkFyDdQAQochXH_jlvxUdJkVtv_wNwF3-QvEPL4eb1HMo_7Y20DV97Uf2O-A'
+const token: string = process.env.NEXT_PUBLIC_AUTH_TOKEN as string
 
 function Index() {
   const { user, error, isLoading } = useUser();
